0e7df817ce916807e79271cacd32aeeb3662af11
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-06-13 15:19:30 +0200

    modify socket context

--- src/components/Notifications/NotificationsNav.js
+++ src/components/Notifications/NotificationsNav.js
@@ -1,161 +1,163 @@
-import React, {useEffect, useState} from 'react'
-import Link from 'next/link'
-import clsx from 'clsx'
-import IconButton from '@material-ui/core/IconButton'
-import NotificationsIcon from '@material-ui/icons/Notifications'
-import {CircularProgress} from "@material-ui/core";
-import {fetchNotifications, removeNotification} from "../../services/NotificationsService";
-import {makeStyles} from "@material-ui/styles";
-import useSocket from '../../hooks/useSocket';
-import { useAuth } from '../../context/AuthProvider';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import clsx from 'clsx';
+import IconButton from '@material-ui/core/IconButton';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { CircularProgress } from '@material-ui/core';
+import { fetchNotifications, removeNotification } from '../../services/NotificationsService';
+import { makeStyles } from '@material-ui/styles';
+import { useSocket } from '../../context/SocketContext';
 
 const useStyles = makeStyles(() => ({
-    '@global': {
-        '*::-webkit-scrollbar': {
-          width: '0.4em'
-        },
-        '*::-webkit-scrollbar-track': {
-            background: '#fff',
-          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
-        },
-        '*::-webkit-scrollbar-thumb': {
-          backgroundColor: 'rgba(0,0,0,.6)',
-        }
+  '@global': {
+    '*::-webkit-scrollbar': {
+      width: '0.4em',
     },
-    wrapper: {
-        border: 'none !important',
-        boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1) !important',
-        borderRadius: '5px !important',
-        paddingLeft: '16px !important',
-        maxHeight: 300,
-        overflowY: "scroll"
+    '*::-webkit-scrollbar-track': {
+      background: '#fff',
+      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
     },
-    notification: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+    '*::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.6)',
+    },
+  },
+  wrapper: {
+    border: 'none !important',
+    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1) !important',
+    borderRadius: '5px !important',
+    paddingLeft: '16px !important',
+    maxHeight: 300,
+    overflowY: 'scroll',
+  },
+  notification: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
 
-        '&:not(:last-child)': {
-            marginBottom: 8
-        }
+    '&:not(:last-child)': {
+      marginBottom: 8,
     },
-    readIndicator: {
-        width: 10,
-        height: 10,
-        background: 'linear-gradient(180deg, #0070F3 -21.43%, #00D9D9 116.67%)',
-        borderRadius: '50%',
-        opacity: 1,
-        margin: 5
-    },
-    opened: {
-        opacity: 0.3
-    },
-    badge: {
-        position: "absolute",
-        top: "10%",
-        right: "20%",
-        width: 10,
-        height: 10,
-        borderRadius: "50%",
-        background: "red",
-        background: 'linear-gradient(180deg, #0070F3 -21.43%, #00D9D9 116.67%) !important'
-    }
-}))
+  },
+  readIndicator: {
+    width: 10,
+    height: 10,
+    background: 'linear-gradient(180deg, #0070F3 -21.43%, #00D9D9 116.67%)',
+    borderRadius: '50%',
+    opacity: 1,
+    margin: 5,
+  },
+  opened: {
+    opacity: 0.3,
+  },
+  badge: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 20,
+    height: 20,
+    borderRadius: '50%',
+    background: 'linear-gradient(180deg, #0070F3 -21.43%, #00D9D9 116.67%) !important',
+  },
+  counts: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontSize: 14,
+  },
+}));
 
 const NotificationsNav = ({ isOpen, keyName, toggle }) => {
-    const classes = useStyles()
-    const socket = useSocket()
-    const { authenticatedUser } = useAuth()
-    const [notifications, setNotifications] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [isLoaded, setIsLoaded] = useState(false)
-    const [isChecked, setIsChecked] = useState(false)
+  const classes = useStyles();
+  const {
+    isNotificationChecked,
+    notificationsChecked,
+    notifications,
+    setNotifications,
+    notificationCounts,
+    setNotificationCounts,
+  } = useSocket();
 
-    useEffect(() => {
-        setIsLoading(true)
-        fetchNotifications()
-            .then(res => {
-                if(res && res.pings) {
-                    setNotifications(res.pings)
-                    const newNotifications = res.pings.filter(item => !item.opened)
-                    if(newNotifications.length > 0) {
-                        if(!isOpen) setIsChecked(false)
-                    } else setIsChecked(true)
-                }
-                setIsLoaded(true)
-            })
-            .finally(() => setIsLoading(false))
-    }, [isOpen])
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-        if (socket){
-            if(isOpen) {
-                socket.emit('OPENED_NOTIFICATION', { user: authenticatedUser.getID })
-                setIsChecked(true)
-            }
-            socket.on('GET_NOTIFICATION', (data) => {
-                setIsChecked(false)
-            })
+  useEffect(() => {
+    setIsLoading(true);
+    fetchNotifications()
+      .then((res) => {
+        if (res && res.pings) {
+          setNotifications(res.pings);
+          const newNotifications = res.pings.filter((item) => !item.opened);
+          if (newNotifications.length > 0) {
+            setNotificationCounts(newNotifications.length);
+          }
         }
+        setIsLoaded(true);
+      })
+      .finally(() => setIsLoading(false));
+  }, [isOpen]);
 
-    }, [socket, isOpen])
+  const handleRemovePing = (pingId) => {
+    setIsLoading(true);
+    removeNotification(pingId)
+      .then((res) => {
+        if (res) setNotifications(notifications.filter((item) => item._id !== pingId));
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-    const handleRemovePing = (pingId) => {
-        setIsLoading(true)
-        removeNotification(pingId).then(res => {
-            if(res) setNotifications(notifications.filter((item) => item._id !== pingId))
-        }).finally(() => setIsLoading(false))
-    }
-
-    return (
-        <li
-            className={clsx('nav-item', 'navbar_icon')}
+  return (
+    <li className={clsx('nav-item', 'navbar_icon')}>
+      <div className="dropdown show">
+        <IconButton
+          color="inherit"
+          data-toggle="dropdown-notifications"
+          aria-haspopup="true"
+          aria-expanded="true"
+          id="dropdownMenu2"
+          onClick={() => {
+            toggle(keyName);
+            notificationsChecked(true);
+          }}
         >
-            <div className="dropdown show">
-                <IconButton color="inherit"
-                    data-toggle="dropdown-notifications"
-                    aria-haspopup="true"
-                    aria-expanded="true"
-                    id="dropdownMenu2"
-                    onClick={() => {
-                        toggle(keyName)
-                        setIsChecked(true)
-                    }}>
-                    <NotificationsIcon/>
-                    {!isChecked && !isOpen && <span className={classes.badge} />}
-                </IconButton>
+          <NotificationsIcon />
+          {!isNotificationChecked && !isOpen && notificationCounts > 0 && (
+            <span className={classes.badge}>
+              <div className={classes.counts}>{notificationCounts}</div>
+            </span>
+          )}
+        </IconButton>
 
-                <div id="dropdown-notifications"
-                    className={clsx('dropdown-menu', isOpen && 'show', classes.wrapper)}>
-                    <div>
-                        {isLoading && <div className="d-flex justify-content-center align-items-center"><CircularProgress /></div> }
-                        {
-                            !notifications.length && !isLoading && isLoaded && (
-                                <div className="text-podpiska">
-                                    <span>You are welcome on Kargain</span>
-                                </div>
-                            )
-                        }
+        <div id="dropdown-notifications" className={clsx('dropdown-menu', isOpen && 'show', classes.wrapper)}>
+          <div>
+            {isLoading && (
+              <div className="d-flex justify-content-center align-items-center">
+                <CircularProgress />
+              </div>
+            )}
+            {!notifications.length && !isLoading && isLoaded && (
+              <div className="text-podpiska">
+                <span>You are welcome on Kargain</span>
+              </div>
+            )}
 
-                        {
-                            !!notifications.length && !isLoading && isLoaded && (
-                                <ul style={{ listStyle: 'none' }}>
-                                    {notifications.map(({ message, action, opened, _id }) => (
-                                        <li key={_id} className={classes.notification}>
-                                            <Link href={action || ''}>{message}</Link>
-                                            <IconButton onClick={() => handleRemovePing(_id)}>
-                                                <span className={clsx(classes.readIndicator, opened && classes.opened)} />
-                                            </IconButton>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )
-                        }
-                    </div>
-                </div>
-            </div>
-        </li>
-    )
-}
+            {!!notifications.length && !isLoading && isLoaded && (
+              <ul style={{ listStyle: 'none' }}>
+                {notifications.map(({ message, action, opened, _id }) => (
+                  <li key={_id} className={classes.notification}>
+                    <Link href={action || ''}>{message}</Link>
+                    <IconButton onClick={() => handleRemovePing(_id)}>
+                      <span className={clsx(classes.readIndicator, opened && classes.opened)} />
+                    </IconButton>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
-export default NotificationsNav
+export default NotificationsNav;
--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,49 +1,75 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import io from 'socket.io-client'
-import { api } from '../config/config'
-import { useAuth } from './AuthProvider'
+import { createContext, useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { api } from '../config/config';
+import { useAuth } from './AuthProvider';
 
-const server = api.slice(0, -3)
-const socketIo = io(server, { autoConnect: false })
+const server = api.slice(0, -3);
+const socketIo = io(server, { autoConnect: false });
 
-const socketContext = createContext()
+const defaultContext = {
+  socket: null,
+  isConnected: false,
+  isNotificationChecked: false,
+  notifications: [],
+  notificationCounts: 0,
+  messages: [],
+  notificationsChecked: () => {},
+  setNotifications: () => {},
+  setNotificationCounts: () => {},
+};
 
-export function SocketProvider({ children }) {
-    const socket = useProvideSocket();
-    return <socketContext.Provider value={socket}>{children}</socketContext.Provider>
-}
+const socketContext = createContext(defaultContext);
 
+export const SocketProvider = ({ children }) => {
+  const { isAuthenticated, authenticatedUser } = useAuth();
+  const [socketState, setSocketState] = useState(defaultContext);
+
+  useEffect(() => {
+    const { socket } = socketState;
+    if ((socket && socket.connected) || !socketIo || !isAuthenticated) return;
+    socketIo.auth = { userId: authenticatedUser.getID };
+    socketIo.connect();
+    setSocketState({ ...socketState, socket: socketIo, isConnected: true });
+
+    return function cleanup() {
+      socketIo.disconnect();
+    };
+  }, [socketState.socket, isAuthenticated]);
+
+  useEffect(() => {
+    const { isConnected, socket } = socketState;
+    if (isConnected) {
+      socket.on('PING', (data) => console.log(data));
+      socket.on('GET_NOTIFICATION', (notifications) => {
+        setNotifications(notifications.data);
+        setNotificationCounts(notifications.counts);
+      });
+    }
+  }, [socketState.socket]);
+
+  const setNotifications = (data) => {
+    setSocketState({ ...notifications, data });
+  };
+
+  const setNotificationCounts = (counts) => {
+    setSocketState({ ...notificationCounts, counts });
+  };
+
+  const notificationsChecked = () => {
+    const { socket } = socketState;
+    if (socket) {
+      socket.emit('OPENED_NOTIFICATION', { user: authenticatedUser.getID });
+      setSocketState({ ...socketState, isNotificationChecked: true });
+    }
+  };
+
+  return <socketContext.Provider value={{ ...socketState, notificationsChecked }}>{children}</socketContext.Provider>;
+};
 
 export const useSocket = () => {
-    return useContext(socketContext)
-}
-
-const useProvideSocket = () => {
-    const [socket, setSocket] = useState(null)
-    const [isConnected, setConnected] = useState(false)
-    const { isAuthenticated, authenticatedUser } = useAuth()
-    useEffect(() => {
-
-        if ((socket && socket.connected) || !socketIo || !isAuthenticated) return;
-        socketIo.auth = { userId: authenticatedUser.getID }
-        socketIo.connect()
-        setConnected(true)
-        setSocket(socketIo)
-
-        return function cleanup() {
-            socketIo.disconnect()
-        }
-
-    }, [socket, isAuthenticated])
-
-    useEffect(() => {
-        if (isConnected) {
-            socket.on('PING', (data) => console.log(data))
-        }
-    }, [socket])
-
-    return {
-        socket,
-        isConnected
-    }
-}
+  const context = useContext(socketContext);
+  if (context === undefined) {
+    throw new Error('socketContext must be used within an SocketProvider');
+  }
+  return context;
+};
