e7a491d7598758e5aec3b9b36c79cb087e49fd27
Roberto Sumoza <robertosumoza@gmail.com>
2021-06-28 12:28:42 -0400

    [refactor] Cleaning metamask connection component.

--- src/connectors.js
+++ src/connectors.js
@@ -17,9 +17,11 @@ const RPC_URLS = {
     4: "https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213"
 };
 
-export const injected = new InjectedConnector({
-    supportedChainIds: [1, 3, 4, 5, 42]
-});
+export const injected = new InjectedConnector(
+//     {
+//     supportedChainIds: [1, 3, 4, 5, 42, 5777]
+// }
+);
 
 export const network = new NetworkConnector({
     urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
