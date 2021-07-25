9787069b31640b6b6fd31d1e32fb65933470897b
Damian <damianclluch@gmail.com>
2021-07-25 20:33:23 +0200

    done

--- src/connectors.js
+++ src/connectors.js
@@ -2,5 +2,7 @@ import { InjectedConnector } from "@web3-react/injected-connector"
 
 
 
-export const injected = new InjectedConnector({ supportedChainIds: [97] })
+export const injected = new InjectedConnector({
+    supportedChainIds: process.env.NODE_ENV === 'production' ? [56] : [97]
+})
 
