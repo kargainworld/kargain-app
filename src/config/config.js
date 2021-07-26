31e7331aa6b7fb2357d1ca0676408ee3f9429b1a
Damian <damianclluch@gmail.com>
2021-07-26 17:54:45 +0200

    done

--- src/config/config.js
+++ src/config/config.js
@@ -24,6 +24,6 @@ module.exports = {
         }
     },
     contract: {
-        KARGAIN_ADDRESS: process.env.NEXT_PUBLIC_KARGAIN_ADDRESS
+        KARGAIN_ADDRESS: '0x3264008f1845a7c7f6e0d054B5d035e045483a2F'
     }
 }
