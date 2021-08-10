ce474f40635bd0b7546ed20370639096533030c1
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-08-10 03:13:01 -0400

    done kar-45

--- src/components/Products/car/Step1_CarDetails.js
+++ src/components/Products/car/Step1_CarDetails.js
@@ -102,10 +102,10 @@ const Step1CarDetails = ({ onSubmitStep, prevStep }) => {
                 <Col sm={12} md={6}>
                     <Row>
                         <Col>
-                            <FieldWrapper label={t(`vehicles:${mileageType?.label}`)}>
-                                <NumberInput
-                                    name="mileage"
-                                    placeholder={`20000 ${mileageType?.value}`}
+                            <FieldWrapper label={t('vehicles:type')}>
+                                <SelectInput
+                                    name="mileageType"
+                                    options={formData.mileageType}
                                     control={control}
                                     errors={errors}
                                     rules={{ required: t('form_validations:required') }}
@@ -113,13 +113,13 @@ const Step1CarDetails = ({ onSubmitStep, prevStep }) => {
                             </FieldWrapper>
                         </Col>
                         <Col>
-                            <FieldWrapper label={t('vehicles:type')}>
-                                <SelectInput
-                                    name="mileageType"
-                                    options={formData.mileageType}
+                            <FieldWrapper label={t(`vehicles:${mileageType?.label}`)}>
+                                <NumberInput
+                                    name="mileage"
+                                    placeholder={`20000 ${mileageType?.value}`}
                                     control={control}
                                     errors={errors}
-                                    selected={'km'}
+                                    rules={{ required: t('form_validations:required') }}
                                 />
                             </FieldWrapper>
                         </Col>
