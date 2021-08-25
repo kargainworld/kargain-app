53653ba50a03b00a11361814a28c7dcc7f896e80
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-08-26 03:34:54 +0900

    change icon in city btn in searchpage

--- src/components/Filters/Advanced/vehicles/CarFilters.js
+++ src/components/Filters/Advanced/vehicles/CarFilters.js
@@ -341,7 +341,7 @@ const CarFilters = ({ control, watch, errors, ...props }) => {
 
                 <ButtonDropdown isOpen={dropdownOpen8} toggle={toggle8} className={clsx(classes.buttondropdown)} >
                     <DropdownToggle caret>
-                        <Emoji name="automobile" width="12" style={{marginLeft: '5px', marginRight: '10px',}}/>
+                        <Emoji name="house" width="12" style={{marginLeft: '5px', marginRight: '10px',}}/>
                         {t('vehicles:address')}
                     </DropdownToggle>
                     <DropdownMenu>
