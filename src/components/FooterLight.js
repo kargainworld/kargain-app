06e36040807d420bb030365c48a7f9f77dddad9f
Facundo De Lorenzo <facundo.delorenzo@gmail.com>
2021-11-16 11:00:59 -0300

    Fix footer centering

--- src/components/FooterLight.js
+++ src/components/FooterLight.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next-translate/Link';
-import { Nav, Navbar } from 'reactstrap';
+import { Nav, Navbar, NavItem } from 'reactstrap';
 import useTranslation from 'next-translate/useTranslation';
 import { makeStyles } from '@material-ui/core/styles';
 import DropdownSwitchLangFlags from './Locales/DropdownSwitchLang';
@@ -10,22 +10,25 @@ import { Emoji } from 'react-apple-emojis';
 
 const useStyles = makeStyles((theme) => ({
   footerLinks: {
+    alignItems: 'center',
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'row',
+    marginTop: 40,
     marginBottom: 30,
     [theme.breakpoints.down('xs')]: {
       marginBottom: 16,
     },
-  },
-  footerContainer: {
-    marginBottom: 16,
   },
   link: {
     color: '#999999',
     fontWeight: 'normal',
     textDecoration: 'none !important',
     fontSize: '14px',
+  },
+  navItem: {
+    marginRight: '10px',
+    textAlign: 'center',
   },
 }));
 
@@ -50,33 +53,30 @@ const FooterLight = () => {
 
   return (
     <footer>
-      <div className={classes.footerContainer}>
-        <Navbar>
-          <Nav navbar className={classes.footerLinks}>
-            {isMobile && (
-              <li key={'kargain-world-link'} className="mx-2" style={{ textAlign: 'center' }}>
-                <Link href="https://kargain.world" prefetch={false}>
-                  <a target="_blank" variant="text" style={{ color: '#2C6BFC', marginRight: '10px' }}>
-                    <Emoji name="globe-with-meridians" width={13} />
-                  </a>
+      <Nav navbar className={classes.footerLinks}>
+        {isMobile && (
+          <NavItem className={classes.navItem}>
+            <Link href="https://kargain.world" prefetch={false}>
+              <a target="_blank" variant="text" style={{ color: '#2C6BFC' }}>
+                <Emoji name="globe-with-meridians" width={13} />
+              </a>
+            </Link>
+          </NavItem>
+        )}
+        {links &&
+          links.map((link, index) => {
+            return (
+              <NavItem className={classes.navItem}>
+                <Link href={link.link}>
+                  <a className={classes.link}>{link.label}</a>
                 </Link>
-              </li>
-            )}
-            {links &&
-              links.map((link, index) => {
-                return (
-                  <li key={index} className="mx-2" style={{ textAlign: 'center' }}>
-                    <Link href={link.link}>
-                      <a className={classes.link}>{link.label}</a>
-                    </Link>
-                  </li>
-                );
-              })}
-
-            <DropdownSwitchLangFlags dropdownStyle={{ top: 'unset', bottom: '-40px' }} />
-          </Nav>
-        </Navbar>
-      </div>
+              </NavItem>
+            );
+          })}
+        <NavItem>
+          <DropdownSwitchLangFlags />
+        </NavItem>
+      </Nav>
     </footer>
   );
 };
--- src/components/Locales/DropdownSwitchLang.js
+++ src/components/Locales/DropdownSwitchLang.js
@@ -1,128 +1,87 @@
-import React, { useState } from 'react'
-import clsx from 'clsx'
-import { useRouter } from 'next/router'
-import Link from 'next-translate/Link'
-import i18nConfig from '../../../i18n.json'
-import useTranslation from 'next-translate/useTranslation'
-import startsWithLang from 'next-translate/_helpers/startsWithLang'
-import Button from "@material-ui/core/Button";
-import {Menu, MenuItem} from "@material-ui/core";
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { useRouter } from 'next/router';
+import Link from 'next-translate/Link';
+import i18nConfig from '../../../i18n.json';
+import useTranslation from 'next-translate/useTranslation';
+import startsWithLang from 'next-translate/_helpers/startsWithLang';
+import Button from '@material-ui/core/Button';
+import { Menu, MenuItem } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {makeStyles} from "@material-ui/styles";
+import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(() => ({
   button: {
-      background: '-webkit-linear-gradient(180deg, #DB00FF 0%, #5200FF 100%)',
-      '-webkit-background-clip': 'text',
-      '-webkit-text-fill-color': 'transparent',
-      display: 'flex',
-
-      '&:hover': {
-        opacity: 0.8
-      }
+    whiteSpace: 'nowrap',
+    background: '-webkit-linear-gradient(180deg, #DB00FF 0%, #5200FF 100%)',
+    '-webkit-background-clip': 'text',
+    '-webkit-text-fill-color': 'transparent',
+    display: 'flex',
+    padding: 0,
+    minWidth: 'auto',
+    '&:hover': {
+      opacity: 0.8,
+    },
   },
   arrowDown: {
     fontSize: '26px',
     lineHeight: 1,
-    marginTop: -10
-  }
-}))
+    marginTop: -10,
+  },
+}));
 
-const DropdownSwitchLang = ({dropdownStyle}) => {
-    const classes = useStyles()
-    const router = useRouter()
-    const { allLanguages, allLanguagesLabel } = i18nConfig
-    const { lang } = useTranslation()
-    const [open, setOpen] = useState(false)
-    const replaceLang = href => startsWithLang(href, allLanguages)
-        ? href.split('/').filter(part => part !== lang).join('/') || '/'
-        : href
-    const [anchorEl, setAnchorEl] = React.useState(null);
+const DropdownSwitchLang = () => {
+  const classes = useStyles();
+  const router = useRouter();
+  const { allLanguages, allLanguagesLabel } = i18nConfig;
+  const { lang } = useTranslation();
+  const replaceLang = (href) =>
+    startsWithLang(href, allLanguages)
+      ? href
+          .split('/')
+          .filter((part) => part !== lang)
+          .join('/') || '/'
+      : href;
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-      <div>
-          <Button
-            className={classes.button}
-            aria-controls="language-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            文A {' '}
-              <span className={classes.arrowDown}>
-                <ExpandMoreIcon
-                    style={{
-                        position: "relative",
-                        top: 3,
-                        fontSize: "20px",
-                        color: '#9e6ffa'
-                    }}
-                />
-              </span>
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-              {allLanguages && allLanguages.map((lng, index) => {
-                  if (lng === lang) return null
-                  return (
-                      <MenuItem key={index} onClick={handleClose}>
-                          <Link
-                            href={replaceLang(router.asPath)}
-                            prefetch={false}
-                            lang={lng}
-                          >
-                            <a>{allLanguagesLabel[lng]}</a>
-                          </Link>
-                      </MenuItem>
-                  )
-              })}
-          </Menu>
-      </div>
-    );
+  return (
+    <>
+      <Button className={classes.button} aria-controls="language-menu" aria-haspopup="true" onClick={handleClick}>
+        文A{' '}
+        <span className={classes.arrowDown}>
+          <ExpandMoreIcon
+            style={{
+              position: 'relative',
+              top: 3,
+              fontSize: '20px',
+              color: '#9e6ffa',
+            }}
+          />
+        </span>
+      </Button>
+      <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+        {allLanguages &&
+          allLanguages.map((lng, index) => {
+            if (lng === lang) return null;
+            return (
+              <MenuItem key={index} onClick={handleClose}>
+                <Link href={replaceLang(router.asPath)} prefetch={false} lang={lng}>
+                  <a>{allLanguagesLabel[lng]}</a>
+                </Link>
+              </MenuItem>
+            );
+          })}
+      </Menu>
+    </>
+  );
+};
 
-    return (
-        <li className="nav-item navbar-dropdown p-2" data-dropdown="dropdownLocale">
-            <span className="dropdown-toggler rounded-circle" onClick={() => setOpen(open => !open)}
-                style={{ width: '30px' }}> {allLanguagesLabel[lang]}
-                <i className={clsx('ml-2', 'arrow_nav', open ? 'is-bottom' : 'is-top')}/>
-            </span>
-            <ul
-                id="dropdownLocale"
-                className={clsx('dropdown', open && 'show')}
-                style={{ minWidth: 'unset', ...dropdownStyle}}>
-                {allLanguages && allLanguages.map((lng, index) => {
-                    if (lng === lang) return null
-                    return (
-                        <li key={index} className="px-0 dropdown-item">
-                            <Link
-                                href={replaceLang(router.asPath)}
-                                prefetch={false}
-                                lang={lng}>
-                                <a className="nav-link text-left">
-                                    <div className="dropdown-toggler" onClick={() => setOpen(open => !open)}>
-                                        <span> {allLanguagesLabel[lng]} </span>
-                                    </div>
-                                </a>
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </li>
-    )
-}
-
-
-export default DropdownSwitchLang
+export default DropdownSwitchLang;
